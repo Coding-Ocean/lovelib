19e97bf09acf5126896d592d9e26bee98899b8ea
Coding-Ocean <eijo524@gmail.com>
2023-09-08 11:45:48 +0900

    mod window.h

--- window.h
+++ window.h
@@ -5,10 +5,9 @@ void closeWindow();
 bool escKeyPressed();
 void hideCursor();
 void showCursor();
+float getWidth();
+float getHeight();
 
 void createWindow(const char* appName, int windowWidth, int windowHeight);
 void destroyWindow();
-
-float getWidth();
-float getHeight();
 
